5843850a1e0ef336734aa8c977e6928fb8332240
Augusto Cesar <acs-03@hotmail.com>
2022-09-29 16:57:13 -0300

    Flyweight

---
+++ src/main/java/br/com/design_patters/structural/flyweight/Client.java
@@ -0,0 +1,18 @@
+package main.java.br.com.design_patters.structural.flyweight;
+
+import main.java.br.com.design_patters.structural.flyweight.service.MusicService;
+
+public class Client {
+
+	public static void main(String[] args) {
+		MusicService player = new MusicService();
+		player.listenMusic("User A", "The Unfforgiven;Mettalica;300");
+		player.listenMusic("User A", "The Unfforgiven;Mettalica;300");
+		player.listenMusic("User A", "The Unfforgiven;Mettalica;300");
+		player.listenMusic("User A", "Be Yourself;Audioslave;200");
+		player.listenMusic("User B", "The Unfforgiven;Mettalica;300");
+		player.listenMusic("User B", "Be Yourself;Audioslave;200");
+
+		player.report();
+	}
+}
